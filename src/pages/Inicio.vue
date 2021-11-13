ed4e463f17be7da9817b596c7b8b274c1bf104a6
Mario <oiram.251170@gmail.com>
2021-11-13 16:29:53 -0600

    Nuevo Proyecto
    Se crea proyecto

---
+++ src/pages/Inicio.vue
@@ -0,0 +1,20 @@
+<template>
+    <q-page class="flex flex-center">
+        <h5>{{ titulo }}</h5>
+    </q-page>
+</template>
+
+<script>
+    export default {
+        name: "Inicio",
+        data() {
+            return {
+                titulo: "Inicio"
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
